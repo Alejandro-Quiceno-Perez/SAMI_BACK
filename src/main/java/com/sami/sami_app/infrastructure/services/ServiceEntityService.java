bc2568445af1580ffd99b3cf4d40c9b27d538307
Alejo Perez <alejo.perez2905@gmail.com>
2024-05-09 20:59:31 -0500

    ServiceEntityService updated

--- src/main/java/com/sami/sami_app/infrastructure/services/ServiceEntityService.java
+++ src/main/java/com/sami/sami_app/infrastructure/services/ServiceEntityService.java
@@ -1,27 +1,43 @@
 package com.sami.sami_app.infrastructure.services;
+import com.sami.sami_app.domain.entities.Service;
 
 import com.sami.sami_app.api.dto.request.ServiceEntityRequest;
 import com.sami.sami_app.api.dto.response.ServiceEntityResponse;
 import com.sami.sami_app.infrastructure.abstract_services.IServiceEntityService;
 import com.sami.sami_app.util.enums.StatusService;
+
+import main.java.com.sami.sami_app.util.enums.SortType;
+
 import org.springframework.data.domain.Page;
+
 
 import java.util.List;
 
 public class ServiceEntityService implements IServiceEntityService {
+
+    @Autowired
+    private final ServiceEntityRepository serviceEntityRepository;
+
+
     @Override
-    public Page<ServiceEntityResponse> getAll(int page, int size) {
+    public Page<ServiceEntityResponse> getAll(int page, int size,SortType sortType) {
+        if (page < 0) page = 0;
+        
         return null;
     }
 
+
+    // Metodo para buscar por ID
     @Override
     public ServiceEntityResponse getById(Long id) {
-        return null;
+        return this.entityToResponse(this.find(id));
     }
 
+    // Metodo para crear un servicio 
     @Override
     public ServiceEntityResponse create(ServiceEntityRequest serviceEntityRequest) {
-        return null;
+        Service service = this.requestToEntity(serviceEntityRequest);
+        return this.entityToResponse(this.serviceEntityRepository.save(service));
     }
 
     @Override
@@ -37,5 +53,30 @@ public class ServiceEntityService implements IServiceEntityService {
     @Override
     public List<ServiceEntityRequest> search(StatusService statusService) {
         return null;
+    }
+
+    // 
+    private ServiceEntityResponse entityToResponse (Service entity) {
+        return ServiceEntityResponse.builder()
+            .idService(entity.getIdService())
+            .latidudeLocation(entity.getLatidudeLocation())
+            .longitudeLocation(entity.getLongitudeLocation())
+            .statusService(entity.getStatusService())
+            .anamnesis(entity.getAnamnesis())
+            .build();
+    }
+
+    private Service requestToEntity (ServiceEntityRequest request) {
+        return Service.builder()
+            .latidudeLocation(request.getLatidudeLocation())
+            .longitudeLocation(request.getLongitudeLocation())
+            .statusService(request.getStatusService())
+            .anamnesis(request.getAnamnesis())
+            .build();
+    }
+
+    private Service find(Long id) {
+        return this.serviceEntityRepository.find(id)
+        .orElseThrow(() -> new RuntimeException("No se encontro el servicio"));
     }
 }
